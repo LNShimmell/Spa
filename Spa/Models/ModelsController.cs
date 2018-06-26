af49e4d4d40863e736b4d0a2a97a228394224e82
Larry Shimmell <larryandshimmell@gmail.com>
2018-06-26 14:26:15 -0400

    First Commit of Spa

---
+++ Spa/Models/ModelsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Spa.Models
+{
+	public class ModelsController : DbContext {
+		public ModelsController() :base(){}
+
+		public DbSet<Customer> Customers { get; set; }
+		public DbSet<Employee> Employees { get; set; }
+		public DbSet<Product> Products { get; set; }
+    }
+}
